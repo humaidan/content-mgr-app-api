4ffc5a40a419509a6b0e4a7f587889d0354554d8
humaidan <humaidan@gmail.com>
2023-05-25 20:48:23 +0300

    ch05-05

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 
+const express = require("express");
+const app = express();
+const PORT = 3001;
 
-console.log("Server is running ...")
+const path = require("path");
+const pathToFile = path.resolve("./data.json")
+
+console.log(pathToFile);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
+app.get("/resources", (req, res) => {
+  res.send("Hello resources");
+})
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port: " + PORT);
+})
